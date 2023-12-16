e5fde5bebbb7abe1166dadaa4f447a8e083ff7e2
Dusan Trtica <dusantrtica@Admins-MacBook-Pro.local>
2023-12-16 12:44:44 +0100

    add header

---
+++ src/components/Picker/PickerHeader.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import smileys_people from '../../assets/svgs/smileys_people.svg'
+import animals_nature from '../../assets/svgs/animals_nature.svg'
+import food_drink from '../../assets/svgs/food_drink.svg'
+import activities from '../../assets/svgs/activities.svg'
+import travel_places from '../../assets/svgs/travel_places.svg'
+import objects from '../../assets/svgs/objects.svg'
+import symbols from '../../assets/svgs/symbols.svg'
+import flags from '../../assets/svgs/flags.svg'
+import recent from '../../assets/svgs/recent.svg'
+
+import groups from '../../data/groups'
+
+const placeholder = ref('')
+const searchValue = ref('')
+
+const icons = {
+  smileys_people,
+  animals_nature,
+  food_drink,
+  activities,
+  travel_places,
+  objects,
+  symbols,
+  flags,
+  recent
+}
+
+const updateActiveGroup = (groupKey: string) => {}
+</script>
+
+<template>
+  <div class="v3-header">
+    <div class="v3-groups">
+      <button
+        v-for="group of groups"
+        :key="group.key"
+        type="button"
+        class="v3-group"
+        :class="{
+          'v3-is-hidden': !icons[group.key]
+        }"
+        @click="updateActiveGroup(group.key)"
+      >
+        <span :title="group.title" class="v3-icon">
+          <img :src="icons[group.key]" alt="" />
+        </span>
+      </button>
+    </div>
+    <div class="v3-spacing" />
+    <div class="v3-search">
+      <input v-model="searchValue" type="text" :placeholder="placeholder" />
+    </div>
+  </div>
+</template>
---
+++ src/components/Root.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import PickerFooter from './Picker/PickerFooter.vue'
+import PickerBody from './Picker/PickerBody.vue'
+import PickerHeader from './Picker/PickerHeader.vue'
+</script>
+
+<template>
+  <div ref="picker" class="v3-emoji-picker">
+    <PickerHeader />
+    <PickerBody />
+    <PickerFooter />
+  </div>
+</template>
