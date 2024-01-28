5faea03a9aa3285fbbd7d186f99fc00e0bcb38c5
Dusan Trtica <dusan.trtica@hotmail.com>
2024-01-28 18:33:26 +0100

    added benchmark for reading and inserting values

--- src/benchmarks.ts
+++ src/benchmarks.ts
@@ -8,28 +8,46 @@ import { Bench } from 'tinybench'
 
 const bench = new Bench({ time: 100 })
 
+const numberOfItemsToAdd = [1000, 10000, 100000, 1000000, 10000000]
+
 const createCacheAndInsertValues = (cacheSize = 10, numberOfElements = 10) => {
-  return () => {
-    const cache = new LRUCache<number>(cacheSize)
-    for (let i = 0; i < numberOfElements; i++) {
-      cache.put(i, i)
-    }
+  const cache = new LRUCache<number>(cacheSize)
+  for (let i = 0; i < numberOfElements; i++) {
+    cache.put(i, i)
+  }
+  return cache
+}
+
+const caches = numberOfItemsToAdd.reduce((acc: Map<number, LRUCache<number>>, n) => {
+  const cache = createCacheAndInsertValues(n, n)
+  acc.set(n, cache)
+  return acc
+}, new Map<number, LRUCache<number>>())
+
+const insertIntoCacheBench = (bench: Bench) => {
+  for (const n of numberOfItemsToAdd) {
+    bench.add(`Insert ${n} values into cache of size ${n}`, () => {
+      const cache = caches.get(n)
+      if (cache) {
+        cache.put(n + 1, n + 1)
+      }
+    })
   }
 }
 
-const insertIntoCacheBench = (bench: Bench) => {
-  const cacheSize = 10
-  const numberOfItemsToAdd = [1000, 10000, 100000, 1000000, 10000000]
-
+const readFromCacheBench = (bench: Bench) => {
   for (const n of numberOfItemsToAdd) {
-    bench.add(
-      `Insert ${n} values into cache of size ${cacheSize}`,
-      createCacheAndInsertValues(cacheSize, n)
-    )
+    bench.add(`Read a value from the cache of size ${n}`, () => {
+      const cache = caches.get(n)
+      if (cache) {
+        cache.get(Math.floor(n / 2))
+      }
+    })
   }
 }
 
 insertIntoCacheBench(bench)
+readFromCacheBench(bench)
 
 await bench.warmup() // make results more reliable, ref: https://github.com/tinylibs/tinybench/pull/50
 await bench.run()
